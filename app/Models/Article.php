874893a6e9f7318373d870a0f5db629c39c3f8e2
Ezeanyim henry <ezeanyimhenry@gmail.com>
2025-04-22 23:13:51 +0100

    fix: change id column to UUID

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-    use HasFactory;
+    use HasFactory, HasUuids;
     protected $fillable = [
         'title',
         'content',
@@ -16,7 +17,7 @@ class Article extends Model
         'url',
         'category'
     ];
-    
+
     protected $casts = [
         'published_at' => 'datetime'
     ];
