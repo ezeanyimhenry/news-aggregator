e2565acb164b9ba4c8238c4865a23cb3e7778012
Ezeanyim henry <ezeanyimhenry@gmail.com>
2025-04-24 13:50:16 +0100

    test

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/files', function () {
+    $files = \Illuminate\Support\Facades\Storage::disk('public')->files('custom');
+    dd($files);
+});
